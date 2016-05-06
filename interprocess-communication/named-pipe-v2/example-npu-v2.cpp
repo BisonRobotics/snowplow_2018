04e7951a72b68c51e0ad3f7faa274bb578e64298
JoeyCluett <joesunglassescool@gmail.com>
2016-05-06 14:45:29 -0500

    added example of IPC over named pipe, example-npu-v2.cpp

--- interprocess-communication/named-pipe-v2/example-npu-v2.cpp
+++ interprocess-communication/named-pipe-v2/example-npu-v2.cpp
@@ -1,32 +1,90 @@
 /*
     This code serves to demonstrate the usage of the second generation named pipe utility
-    It spawns multiple threads (described in-depth in a different project). JJC::Thread
-    library used for mutlithreading functionality. In current state, this code will compile
-    but get hung up during runtime. It should be noted that the compiler is told where to
-    look for 'NamedPipeUtility.h', thus the angle brackets instead of quotes.
+    for interprocess communication (IPC). It spawns multiple threads (described in-depth
+    in a different project). JJC::Thread library used for mutlithreading functionality.
+
+    Speed tests--
+    tests performed on personal computer
+        cpu specs:
+        - i5-4590 @ 3.7GHz x4
+        - 16GB RAM
+        - 64KB L1 cache
+        - 256KB L2 cache
+
+        transmit speeds of various data types:
+        - char (8-bit)
+            ~1,212,856 char/sec   || ~1,212,856 bytes/sec
+        - int (32-bit)
+            ~1,245,950 int/sec    || ~4,983,800 bytes/sec
+        - float (32-bit)
+            ~1,245,019 float/sec  || ~4,980,076 bytes/sec
+        - long long int (64-bit integer)
+            ~1,241,773 long/sec   || ~9,934,184 bytes/sec
+        - double (64-bit floating-point)
+            ~1,250,781 double/sec || ~10,006,248 bytes/sec
 */
 
 #include <NamedPipeUtility.h>
+#include <Thread.h>
 #include <iostream>
+
+//speed test runs
+#define maxtransmit 10000000
+
+//change this to whatever primitive you want
+typedef long long int mytype;
 
 using namespace std;
 
+void* write_thread(void*) {
+
+    JJC::NamedPipeUtility<mytype> writer_pipe;            //instantiate pipe object
+    writer_pipe.Simplex_create("/tmp/myfifo-1", true); //specify pipe name and whether it needs to be created
+    writer_pipe.Simplex_open(IS_WRITER);               //specify operations this thread will perform on pipe
+
+    /* continue sending data over pipe until user enters zero */
+    mytype i_num;
+    do {
+        cout << "enter primitive:  ";
+        cin >> i_num;
+        writer_pipe.Simplex_write(&i_num); //single write function
+        usleep(200);
+    } while(i_num != 0);
+
+    /* close and destroy pipe. only one thread needs to destroy the pipe, however both must close their ends */
+    writer_pipe.Simplex_close();
+    writer_pipe.Simplex_destroy();
+}
+
+void* read_thread(void*) {
+
+    JJC::NamedPipeUtility<mytype> reader_pipe;    //instantiate pipe object
+    reader_pipe.Simplex_create("/tmp/myfifo-1", false); //specify pipe name and whether it needs to be created
+    reader_pipe.Simplex_open(IS_READER);                //specify operations this thread will perform on pipe
+
+    /* continue reading data until recieve zero */
+    mytype i_num;
+    do {
+        reader_pipe.Simplex_read(&i_num); //single read function
+        cout << "from read_thread: " << i_num << endl;
+    } while(i_num != 0);
+
+    /* thread closes but does not destroy pipe. pipe destruction is write_thread's job */
+    reader_pipe.Simplex_close();
+}
+
 int main(int argc, char* argv[]) {
 
-    //as the base data type is the same size, JJC::NPU should have no trouble sending data
-    JJC::NamedPipeUtility<int>          writer_pipe;
-    JJC::NamedPipeUtility<unsigned int> reader_pipe;
+    //instantiate 2 thread objects to demonstrate IPC with named pipes
+    JJC::Thread<void> send_thread(write_thread);
+    JJC::Thread<void> rcv_thread(read_thread);
 
-    writer_pipe.Simplex_create("/tmp/myfifo-1", true);
-    reader_pipe.Simplex_create("/tmp/myfifo-1", false);
+    //fork both threads, order doesnt really matter
+    send_thread.forkThread();
+    rcv_thread.forkThread();
 
-    reader_pipe.Simplex_open(IS_READER);
-    writer_pipe.Simplex_open(IS_WRITER);
-
-    writer_pipe.Simplex_close();
-    reader_pipe.Simplex_close();
-    writer_pipe.Simplex_destroy();
+    //wait for thread to finish execution
+    rcv_thread.syncThread();
 
     return 0;
 }
-
