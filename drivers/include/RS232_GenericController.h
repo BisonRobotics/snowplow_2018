9df2879dbf2b5329bc0b3fa7eaf6f7ed3945cc40
Joey Cluett <joesunglassescool@gmail.com>
2017-04-03 02:13:43 -0500

    redid serial library, reorganized a few things again, and started on Makefile

---
+++ drivers/include/RS232_GenericController.h
@@ -0,0 +1,63 @@
+#ifndef __JJC__RS232GENERIC__H__
+#define __JJC__RS232GENERIC__H__
+
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include <iostream>
+
+#define SC SerialController
+
+class SerialController {
+private:
+    termios tty;
+    int fd = -1;
+    bool serialPortSet = false;
+    void parityEnable(void);
+
+public:
+    enum PARITY {
+        ODD, EVEN, OFF
+    };
+
+    enum WORDSIZE {
+        _7, _8
+    };
+
+    enum STOPBITS {
+        _1, _2
+    };
+
+    // constructor that does nothing
+    SerialController(void);
+
+    // constructor with serial port as argument
+    SerialController(const char* serialPort);
+
+    void set_SerialPort(const char* serialPort);
+
+    // read a certain number of bytes from the serial port
+    void readBuffer(char* buffer, int bufSize);
+
+    // write a certain number of bytes from the serial port
+    void writeBuffer(char* buffer, int bufSize);
+
+    // set the read/write speeds for the serial port
+    void set_BaudRate(int baudrate);
+
+    // set odd/even/no parity for this serial port
+    void set_Parity(PARITY parity);
+
+    // set either 7 or 8 bit word for this serial port
+    void set_WordSize(WORDSIZE wordsize);
+
+    // set either 1 or 2 stop bits for this port
+    void set_StopBits(STOPBITS stopbits);
+
+    // start serial communications
+    void start(void);
+};
+
+#endif // __JJC__RS232GENERIC__H__
---
+++ drivers/src/RS232_GenericController.cpp
@@ -0,0 +1,131 @@
+#include <RS232_GenericController.h>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
+#include <iostream>
+
+//#define SC SerialController
+
+SC::SerialController(void) {
+    ;
+}
+
+SC::SerialController(const char* serialPort) {
+    SerialController::set_SerialPort(serialPort);
+}
+
+void SC::set_SerialPort(const char* serialPort) {
+    memset(&tty, 0, sizeof tty);
+    fd = open(serialPort, O_RDWR | O_NOCTTY | O_NDELAY); // pretty standard flags
+    if(tcgetattr(fd, &tty) < 0) {
+        std::cerr << "Error opening file" << std::endl;
+        std::cerr << "Does " << serialPort << " exist?" << std::endl;
+        exit(-1);
+    }
+    serialPortSet = true;
+}
+
+void SC::parityEnable(void) {
+    tty.c_cflag |= PARENB;
+}
+
+void SC::writeBuffer(char* buffer, int bufSize) {
+    write(fd, buffer, bufSize);
+}
+
+void SC::readBuffer(char* buffer, int bufSize) {
+    read(fd, buffer, bufSize);
+}
+
+void SC::set_BaudRate(int baudrate) {
+    cfsetispeed(&tty, baudrate);
+    cfsetospeed(&tty, baudrate);
+}
+
+void SC::set_Parity(SC::PARITY parity) {
+    switch(parity) {
+        case SC::PARITY::EVEN:
+            tty.c_cflag |= PARENB;
+            tty.c_cflag &= ~PARODD;
+            break;
+        case SC::PARITY::ODD:
+            tty.c_cflag |= PARENB;
+            tty.c_cflag |= PARODD;
+            break;
+        case SC::PARITY::OFF: // disable the parity bit
+            tty.c_cflag &= ~PARENB;
+            break;
+        default:
+            std::cerr << "Invalid parity argument" << std::endl;
+            exit(-1); // all error comditions return -1
+    }
+}
+
+void SC::set_StopBits(SC::STOPBITS stopbits) {
+    switch(stopbits) {
+        case SC::STOPBITS::_1:
+            tty.c_cflag &= ~CSTOPB;
+            break;
+        case SC::STOPBITS::_2:
+            tty.c_cflag |= CSTOPB;
+            break;
+        default:
+            std::cerr << "Invalid stop bit argument" << std::endl;
+            exit(-1);
+    }
+}
+
+void SC::set_WordSize(SC::WORDSIZE wordsize) {
+    switch(wordsize) {
+        case SC::WORDSIZE::_7:
+            tty.c_cflag &= ~CSIZE;
+            tty.c_cflag |= CS7;
+            break;
+        case SC::WORDSIZE::_8:
+            tty.c_cflag &= ~CSIZE;
+            tty.c_cflag |= CS8;
+            break;
+        default:
+            std::cerr << "Invalid word size argument" << std::endl;
+            exit(-1);
+    }
+}
+
+void SerialController::start(void) {
+    if(serialPortSet == false) {
+        std::cerr << "Serial port has not been opened" << std::endl;
+        exit(-1);
+    }
+
+    if(tcsetattr(fd, TCSANOW, &tty) != 0) {
+        std::cerr << "Error starting serial communications" << std::endl;
+        std::cerr << "Error code: " << errno << std::endl;
+        exit(-1);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
